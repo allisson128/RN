a63edf77a5fa2380f7dcc24f4b3da3a223d93716
allisson128 <allisson128@gmail.com>
2015-12-09 08:46:23 -0200

    dial

--- main.c
+++ main.c
@@ -181,14 +181,14 @@ int* percepone (int ages, int n, int x[][31],
 	for (j = 0; j < 31; ++j) {
 	  w[j] += n * err * x[i][j];
 	}
+	E = 1
       }
 
-      E += err;
     }
 
     ++t;
 
-  } while (t < ages && E > 0);
+  } while (E > 0);
 
 
   //print new W
